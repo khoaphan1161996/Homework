36951cc9b15382e72b9d255fd507c7aa74df5b0d
khoaphan1161996 <khoa071996@gmail.com>
2020-10-01 08:17:42 +0700

    homework

---
+++ C4EJS-Session8Bonus-HW/Value,Type,Operators/script.js
@@ -0,0 +1,11 @@
+// 1. Radians to Degrees
+// Create a function that takes a n angle in radians and returns the corresponding angle in degrees.
+function radiansToDegrees(rad){
+    console.log((180/3.14)*rad)
+}
+radiansToDegrees(1)
+radiansToDegrees(20)
+radiansToDegrees(50)
+
+// 2. Celsius to Fahrenheit
+// Create a function that takes a n temperature in celsius and returns the corresponding temperature in Fahrenheit.
--- C4EJS-Session9-HW/script.js
+++ C4EJS-Session9-HW/script.js
@@ -19,7 +19,7 @@
 
 // 3.2. Can var variable declared by var, in a function still exists after the
 // function finishes running?
-// Không trừ khi khai báo ngoài hàm
+// Còn 
 
 // 4. You can get DOM (Read) by tag name using getElementsByTagName function, the
 // result you get is NOT a single HTMLElement but a HTMLCollection, which can be
@@ -28,11 +28,11 @@
 // Use getElementsByTagName function to get ALL of the <li>tag. Print/log out the first
 // element (which is <li>Articles</li>)
 // let a = document.getElementsByTagName("li")
-// console.log(a[1].textContent)
+// console.log(a[1])
 // and then use for loop to print ALL of the
 // DOM you get from the function.
 // for(let i =0;i<a.length; i ++){
-//     console.log(a[i].textContent)
+//     console.log(a[i])
 // }
 
 // 5. You can get DOM (Read) by class name using getElementsByClassName function.
@@ -42,11 +42,11 @@
 // Use getElementsByTagName function to get ALL of the <div> tag.Print/log out the
 // second element (which is <div class="singer">Nate Ruess</div>
 // let a = document.getElementsByClassName("singer")
-// console.log(a[1].textContent)
+// console.log(a[1])
 // and then
 // use for loop to print ALL of the DOM you get from the function.
 // for(let i =0;i<a.length;i++){
-//     console.log(a[i].textContent)
+//     console.log(a[i])
 // }
 
 // 6. Learn how to remove (Delete) an HTMLElement using HTMLElement.remove()
@@ -107,45 +107,89 @@
 // let b = prompt("nhập mong muốn của bạn trong năm nay đi")
 // user(a,b)
 
-// // 11. Take a look at the following UI
-// // 11.1. Write an HTML for the UI above, create and link a JS file to it
-// // 11.2. Get (Read) the 'Upper it!!!!
-    let upper_btn = document.getElementById("upper_btn")
-    console.log(upper_btn)
+// 11. Take a look at the following UI
+// 11.1. Write an HTML for the UI above, create and link a JS file to it
+// 11.2. Get (Read) the 'Upper it!!!!
+    // let upper_btn = document.getElementById("upper_btn")
+    // console.log(upper_btn)
 
-// // 11.3. Catch event when 'Upper it!!!!' button is clicked
-// // 11.4. Get(Read) 'Enter your name' input
-    let name_input = document.getElementById("name_input")
-    console.log(name_input)
+// 11.3. Catch event when 'Upper it!!!!' button is clicked
+// 11.4. Get(Read) 'Enter your name' input
+    // let name_input = document.getElementById("name_input")
+    // console.log(name_input)
 
-// // 11.5. Get (Read) user name from the input
-// // 11.6. Convert user’s name to uppercase
-// // 11.7. Get (Read) the ‘Your result will appear here’ tag
-// // 11.8. Update the ‘Your result will appear here’ to the username
+// 11.5. Get (Read) user name from the input
+// 11.6. Convert user’s name to uppercase
+// 11.7. Get (Read) the ‘Your result will appear here’ tag
+// 11.8. Update the ‘Your result will appear here’ to the username
 // uppercase version
-    let result = document.getElementById("result_div")
-    console.log(result)
+    // let result = document.getElementById("result_div")
+    // console.log(result)
 
-    upper_btn.addEventListener('click',function(e){
-        console.log(e.target)
-        result.innerHTML=name_input.value.toUpperCase()
-        console.log(name_input.value.toUpperCase())
-    })
-
-    
-
+    // upper_btn.addEventListener('click',function(e){
+    //     console.log(e.target)
+    //     result.innerHTML=name_input.value.toUpperCase()
+    // })
 
 // 12. Take a look at a list of belongings
 // 12.1. In JS, Initialize a variable named items to store the data above, log/print
 // it out
-// let menu = ["Backpack", "MiBand watch","Ring"]
-// console.log(menu)
+items = ["Backpack", "MiBand watch","Ring"]
+// console.log(items)
 
 // 12.2. In HTML, create an &lt;ul&gt; with some test items. If you have not worked
 // with unordered list before, watch the ‘HTML - List and Table’ video or see this
 // quick example.
 // 12.3. Get (Read) the list from DOM
-// let readlist = document.getElementById("list")
-// console.log(readlist)
+let list = document.getElementById("item_list_ul")
+// console.log(list)
 
 // 12.4. Show ALL of the data in items, each item is a <li> tag
+for(let i = 0 ; i<items.length;i ++){
+    list.insertAdjacentHTML(`beforeend`,`<li><span>${items[i]}</span><button class="btn_remove">Remove</button></li>`)
+}
+document.getElementsByClassName("test_item")[0].remove()
+document.getElementsByClassName("test_item")[0].remove()
+
+// 12.5. Update the UI (HTML) as follows:
+// 12.6. Get the ‘New item’ input and ‘Add’ button
+let input = document.getElementById("item_input")
+let add_btn = document.getElementById("add_btn")
+// console.log(input)
+// console.log(add_btn)
+
+// 12.7. Register ‘Add‘ button click event
+// 12.8. When ‘Add’ button is clicked, the value of ‘New item’ input
+// 12.9. When ‘Add’ button is clicked, add the value of ‘New item’ input to
+// items Array
+// 12.10. Update UI (HTML) to reflect the update of items Array
+// 12.11. Reset the input after adding new item
+function them (callback){
+    add_btn.addEventListener('click', function(){
+        let inputvalue = input.value
+        items.push(inputvalue)
+        console.log(items)
+        list.insertAdjacentHTML(`beforeend`,`<li><span>${inputvalue}</span><button class="btn_remove">Remove</button></li>`)
+        console.log(list)
+        input.value = ""
+        callback()
+    })
+    
+}
+them(xoa)
+xoa()
+// 12.12. Add remove button for each item
+// 12.13. Register to each ‘remove’ buttons’ event 
+// 12.14. Each time a ‘remove’ button clicked, identify the index of the clicked
+// item
+// 12.15. Remove from items based on clicked item index
+// 12.16. Update UI (HTML) to reflect the update of items Array
+function xoa(){
+    let btn_remove = document.getElementsByClassName("btn_remove")
+    for(let i = 0 ; i<btn_remove.length;i++){
+    btn_remove[i].addEventListener('click', function(e){
+        e.target.parentElement.remove()
+    })
+}
+}
+
